80a9111b6df70bc7af20b8d1f3220709c09f4f3f
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2023-04-26 12:31:57 -0500

    patch method to put method

--- src/main/java/com/samtel/clientes/controlller/ClienteRestController.java
+++ src/main/java/com/samtel/clientes/controlller/ClienteRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +63,7 @@ public class ClienteRestController {
     }
   }
 
-  @PatchMapping("/{id}")
+  @PutMapping("/{id}")
   public ResponseEntity<ClienteDTO> updateCliente(@PathVariable Long id,
       @RequestBody ClienteDTO cliente) {
     try {
